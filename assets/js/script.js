c60190603aab3271998e6e3036b2016f6ce9815d
Osama Hendawi <osama.hendawi@hotmail.com>
2024-08-05 15:15:46 +0000

    added add task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
+const input = document.getElementById('inputBox');
 
 function addTask() {
-    let input = document.getElementById('inputBox').value;
+    let newTask = document.getElementById('inputBox').value;
+    let newListItem = document.createElement("li");
+    let list = document.getElementsByTagName('ul');
+    
+    if (newTask == '') {
+        alert('Please enter a new task!');
+    } else {
+    newListItem.textContent = newTask;
+    list[0].appendChild(newListItem);
+    }
 
-    console.log(input);
+    document.getElementById("inputBox").value = "";
 }
