b8a558c94260d27df101c48f3cc4241b7fa95873
Osama Hendawi <osama.hendawi@hotmail.com>
2024-08-06 09:47:38 +0000

    adding ability to delete a task by pressing x

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 - How to clear a text field using JS: https://stackoverflow.com/questions/14837466/clearing-a-text-field-on-button-click
 - Used for help on how to add an HTML element is a class list: https://www.shecodes.io/athena/3913-how-to-add-a-class-to-an-html-element-in-javascript#:~:text=In%20JavaScript%2C%20you%20can%20add,class%20to%20the%20classList%20property.&text=This%20code%20retrieves%20the%20element,myClass"%20to%20the%20classList%20property.
 - How to set a HTML attribute via JavaScript: https://www.w3schools.com/jsref/met_element_setattribute.asp
+- How to use toggle event: https://www.w3schools.com/howto/howto_js_toggle_class.asp
+- How to use the event property: https://www.w3schools.com/jsref/event_target.asp
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,28 @@ const input = document.getElementById('inputBox');
  * Clears add a task for field.
  */
 function addTask() {
+    let deleteTask = document.createElement('span');
     let newTask = document.getElementById('inputBox').value;
     let newListItem = document.createElement("li");
     let list = document.getElementsByTagName('ul');
-    
+
     if (newTask == '') {
         alert('Please enter a new task!');
     } else {
     newListItem.textContent = newTask;
     list[0].appendChild(newListItem);
     newListItem.setAttribute("onclick", "completeTask(event)");
+    deleteTask.innerHTML = '<i class="fa-regular fa-circle-xmark"></i>'
+    newListItem.appendChild(deleteTask);
     }
 
     document.getElementById("inputBox").value = "";
 }
 
+/**
+ * Toggles 'complete' class so clicked on tasks
+ * are marked as completed.
+ */
 function completeTask(event) {
     let completedTask = event.target;
 
